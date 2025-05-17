b3d444bc056b40019aa93d968ec2dfe0c5dbb2d5
Karan S Khedkar <103458258+karanskhedkar@users.noreply.github.com>
2025-05-17 23:00:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 function validEmail(str) {
   //your JS code here.
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(str)
+	
 }
 
 // Do not change the code below.
-const str = prompt("Enter an email address.");
-alert(validEmail(str));
+// const str = prompt("Enter an email address.");
+// alert(validEmail(str));
